90bb6a0280bea6d66fe916d933660cfc599ff87a
Shivansh Sharma <shivansh.report@gmail.com>
2024-12-19 18:55:38 +0530

    Added sprite rendering, weapons and sound effects

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from player import *
 from raycasting import *
 from object_renderer import *
 from sprite_object import *
+from object_handler import *
+from weapon import *
+from sound import *
 
 class Game:
     def __init__(self):
@@ -21,12 +24,18 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
-        self.static_sprite = SpriteObject(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
+
+
+
 
     def update(self):
         self.player.update()
         self.raycasting.update()
-        self.static_sprite.update()
+        self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time= self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() : .1f}')
@@ -34,6 +43,7 @@ class Game:
     def draw(self):
         self.screen.fill('black')
         self.object_renderer.draw()
+        self.weapon.draw()
         # self.map.draw()
         # self.player.draw()
 
@@ -42,6 +52,8 @@ class Game:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            
+            self.player.single_fire_event(event)
 
     def run(self):
         while True:
